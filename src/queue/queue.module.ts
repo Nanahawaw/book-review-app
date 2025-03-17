5c012b285bba23ea5f11969343f08bed000343b8
Nanahawaw <folarinyetunde@gmail.com>
2025-03-17 11:22:16 +0100

    setting up rabbitmq

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
 
+@Global()
 @Module({
-  providers: [QueueService]
+  providers: [QueueService],
+  exports: [QueueService],
 })
 export class QueueModule {}
--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as amqp from 'amqplib';
 @Injectable()
-export class QueueService {}
+export class QueueService implements OnModuleInit, OnModuleDestroy {
+  private connection: amqp.Connection;
+  private channel: amqp.Channel;
+  constructor(private configService: ConfigService) {}
+
+  async onModuleInit() {
+    await this.connect();
+  }
+  async onModuleDestroy() {
+    await this.channel?.close();
+    await this.connection?.close();
+  }
+
+  async connect() {
+    this.connection = await amqp.connect(
+      this.configService.get('rabbitmq.url'),
+    );
+    this.channel = await this.connection.createChannel();
+
+    //define queues
+    await this.channel.assertQueue('review-processing', { durable: true });
+    console.log('Connected to RabbitMQ');
+  }
+
+  async publishToQueue(queue: string, message: any) {
+    return this.channel.sendToQueue(
+      queue,
+      Buffer.from(JSON.stringify(message)),
+      {
+        persistent: true,
+      },
+    );
+  }
+}
